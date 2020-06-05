a24e2ef1df5a7461416c11978361e25ed013aa35
N1ck19 <nickynick2003@yahoo.com>
2020-06-05 19:00:44 -0400

    Made it so that the phrases are shown in a random order

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var phrases = {
+/* var phrases = {
 phrase1: "I said don't press the button.",
 phrase2: "Seriously, stop.",
 phrase3: "I mean it!",
@@ -6,16 +6,26 @@ phrase3: "Are you going to do this all day?",
 phrase4: "No means no!",
 phrase5: "Why don't you listen to me?",
 phrase6: "God, you don't stop!",
-};
+}; */
+
+var phrases = [
+"I said don't press the button.",
+"Seriously, stop.",
+"I mean it!",
+"Are you going to do this all day?",
+"No means no!",
+"Why don't you listen to me?",
+"God, you don't stop!"
+]
 
 function buttonPress() {
   //document.getElementById("changing_text").innerHTML = phrases[(Math.random() * 6) - 1];
   var topText = document.getElementById("changing_text");
-  if (topText.innerHTML == "Don't press the button.") {
-    topText.innerHTML = phrases.phrase1;
-  } else if (topText.innerHTML == phrases.phrase1) {
-    topText.innerHTML = phrases.phrase2;
-  } else if (topText.innerHTML == phrases.phrase2) {
+  //if (topText.innerHTML == "Don't press the button.") {
+  topText.innerHTML = phrases[Math.floor(Math.random() * 7)];
+  //} else if (topText.innerHTML == phrases[Math.floor(Math.random() * 6)]) {
+    //topText.innerHTML = phrases[Math.floor(Math.random() * 6)];
+  /* } /* else if (topText.innerHTML == phrases.phrase2) {
     topText.innerHTML = phrases.phrase3;
   } else if (topText.innerHTML == phrases.phrase3) {
       topText.innerHTML = phrases.phrase4;
@@ -25,5 +35,5 @@ function buttonPress() {
       topText.innerHTML = phrases.phrase6;
   } else if (topText.innerHTML == phrases.phrase6) {
     topText.innerHTML = "Don't press the button."
-  }
+  } */
 };
